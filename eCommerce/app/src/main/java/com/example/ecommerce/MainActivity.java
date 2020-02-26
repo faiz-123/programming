0a9185661ab221de0d7fa167da3824d4efce734e
faiz-123 <60651414+faiz-123@users.noreply.github.com>
2020-02-26 18:38:18 +0530

    eCommerce

--- eCommerce/app/src/main/java/com/example/ecommerce/MainActivity.java
+++ eCommerce/app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.ecommerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 /**
@@ -35,6 +37,14 @@ public class MainActivity extends AppCompatActivity {
 
         joinNowButton =(Button) findViewById(R. id. main_join_now_btn);
         loginButton =(Button) findViewById(R. id. main_login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
